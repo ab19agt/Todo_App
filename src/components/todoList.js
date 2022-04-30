f0c36e8dcb23246cb1122d9745f64aa46f143037
Arun Balachandran <arun@Aruns-MacBook-Air.local>
2022-04-30 18:39:49 +0100

    delete function working

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from "react";
 import { useState, useEffect } from "react";
-import { collection, getDocs, onSnapshot } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 import { db } from "../firebase.config";
 function TodoList(props) {
   useEffect(() => {
@@ -11,9 +11,9 @@ function TodoList(props) {
   const [todo, setTodo] = useState([]);
   const colref = collection(db, "todo");
   function getTodos() {
-    getDocs(colref).then((doc) => {
+    getDocs(colref).then((dos) => {
       setTodo(
-        doc.docs.map((values) => ({
+        dos.docs.map((values) => ({
           id: values.id,
           todos: values.data().Todo,
           inProgress: values.data().inprogress,
@@ -24,16 +24,37 @@ function TodoList(props) {
   function todolists() {
     let v = [];
 
-    for (let i = 0; i < todo.length; i++) {
-      v.push(<li>{todo[i].todos}</li>);
-    }
+    /* for (let i = 0; i < todo.length; i++) {
+      console.log(todo[i].id);
+      v.push(
+        <div>
+          <li>{todo[i].todos}</li>
+          <button id={todo[i].id} onClick={deleteTodo(todo[i].id)}>
+            delete
+          </button>
+        </div>
+      );
+    } */
+
     console.log(v);
     return v;
+  }
+  function deleteTodo(id) {
+    deleteDoc(doc(db, "todo", id));
   }
 
   return (
     <div>
-      <ul>{todolists()}</ul>
+      <ul>
+        {todo.map((dt) => {
+          return (
+            <div>
+              <li>{dt.todos}</li>
+              <button onClick={deleteTodo(dt.id)}>delete</button>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 }
